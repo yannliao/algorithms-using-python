6157fad588ae5a8201c9042b3c089bb290d09072
yannliao <trembleliao@gmail.com>
2016-07-25 16:37:31 +0800

    add priority queue

---
+++ src/priorityQueue.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+    implementation of Priority Queue using list
+"""
+
+
+class PriorityQueue:
+    def __init__(self):
+        self.data = list()
+
+    def __len__(self):
+        return len(self.data)
+
+    def isEmpty(self):
+        return len(self.data) == 0
+
+    def enqueue(self, item, priority):
+        entry = _PriorityNode(self, item, priority)
+        self.data.append(entry)
+
+    def dequeue(self):
+        assert not self.isEmpty(), "can not dequeue from an empty queue"
+        highestPriority = self.data[0].priority
+        highestindex = 0
+        for i in range(len(self)):
+            if self.data[i].priority > highestPriority:
+                highestPriority = self.data[i].priority
+                highestindex = i
+        entry = self.data.pop(highestindex)
+        return entry.item
+
+
+class _PriorityNode:
+    def __init__(self, item, priority):
+        self.item = item
+        self.priority = priority
---
+++ test/test_priorityQueue.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+test code for linked list
+"""
+import context
+import unittest
+from src.priorityQueue import PriorityQueue
+
+
+class TestPriorityQueue(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_enqueue(self):
+        pass
+
+    def test_dequeue(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
